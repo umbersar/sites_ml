107ff8d374d2f157816e8cd175dea902dd99d3bf
u6284513 <u6284513@anu.edu.au>
2020-04-21 14:21:25 +1000

    separate with different files

--- soil.r
+++ soil.r
@@ -38,28 +38,13 @@ library(neuralnet)
 #adabag library
 library(adabag)
 
-#load the soil data
-soil <- read.csv(file = "hr_lr_labm.csv")
-#reduce the amount to 5000
-rSoil <- nrow(soil)
-
 #normalize the labr_value name
 #preprocessing the data
 normalize <- function(x){
   return ((x-min(x))/(max(x)-min(x)))
 }
 
-soil$labr_value <- normalize(as.numeric(soil$labr_value))
-
-#extract three column labm_code labr_value h_texture
-featureSoilTable <- dcast(soil, ﻿agency_code + proj_code + h_texture +  s_id + o_id + h_no + samp_no + labr_no ~ labm_code,value.var = "labr_value")
-
-#drop some of useless columns
-featureSoilTable <- select (featureSoilTable,-c(﻿agency_code,proj_code,s_id,o_id,h_no))
-
-write.csv(featureSoilTable,"featureTable.csv")
-
-read.csv(featureSoilTable,"featureTable.csv")
+featureSoilTable <- read.csv(file = "featureTable.csv")
 #change the NULL to na
 featureSoilTable['h_texture'][featureSoilTable['h_texture'] == "NULL"] <- NA
 
